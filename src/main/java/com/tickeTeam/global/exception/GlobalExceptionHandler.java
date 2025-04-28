0abe0ea8fd3d9f8aa9d7a7018c2dc724685e1551
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-04-29 00:53:18 +0900

    chore: 글로벌 예외 처리 세팅

--- src/main/java/com/tickeTeam/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tickeTeam/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.tickeTeam.global.exception.customException.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +17,14 @@ public class GlobalExceptionHandler {
         log.error(e.getMessage(), e);
         ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INPUT_INVALID_VALUE, e.getBindingResult());
+        log.warn(e.getMessage());
+        return ResponseEntity.status(ErrorCode.INPUT_INVALID_VALUE.getStatus()).body(response);
     }
 
     // @RestController 에서 발생한 에러 핸들러, @Controller 관련 어노테이션이 있어야지만 사용 가능
