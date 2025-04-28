01a984650f2ff24b7db5cd674a8ea1dcf52a0cdd
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-04-28 23:46:27 +0900

    chore: 기초 환경 세팅

---
+++ src/main/java/com/tickeTeam/TickeTeamApplication.java
@@ -0,0 +1,13 @@
+package com.tickeTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickeTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickeTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tickeTeam/TickeTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickeTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickeTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
