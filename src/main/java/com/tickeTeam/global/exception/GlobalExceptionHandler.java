f17b747cfac4a29188867058a678905e8f77b2bf
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-04-29 00:47:59 +0900

    chore: 글로벌 예외 처리 세팅

---
+++ src/main/java/com/tickeTeam/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.tickeTeam.global.exception;
+
+import com.tickeTeam.global.exception.customException.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e){
+        log.error(e.getMessage(), e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // @RestController 에서 발생한 에러 핸들러, @Controller 관련 어노테이션이 있어야지만 사용 가능
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e){
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response = makeErrorResponse(errorCode);
+        log.error(e.getMessage(), e);
+        log.warn(e.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .errorMessage(errorCode.getMessage())
+                .businessCode(errorCode.getCode())
+                .build();
+    }
+}
