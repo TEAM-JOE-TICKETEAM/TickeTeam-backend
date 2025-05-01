31ee89854cca602fc96fd862b76239dcbcea9ae4
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-05-02 02:35:39 +0900

    feat: member 엔티티 구현

---
+++ src/main/java/com/tickeTeam/domain/member/Member.java
@@ -0,0 +1,47 @@
+package com.tickeTeam.domain.member;
+
+import com.tickeTeam.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@SQLRestriction("is_deleted = false")
+@SQLDelete(sql = "UPDATE member SET is_deleted = true WHERE member_id = ?")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name; // 이름
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email; // 사용자 이메일
+
+    @Column(name = "password", nullable = false)
+    private String password; // 비밀번호
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "favorite_team", nullable = false)
+    private Team favoriteTeam; // 응원팀
+
+}
