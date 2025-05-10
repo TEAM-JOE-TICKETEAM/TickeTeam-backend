41327d314ab16a3023ba7d185ce2eb8baa26efc0
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-05-10 18:37:38 +0900

    feat: 실행일 기준 7일 내의 경기 좌석 데이터 생성 로직 구현

--- src/main/java/com/tickeTeam/domain/game/repository/GameRepository.java
+++ src/main/java/com/tickeTeam/domain/game/repository/GameRepository.java
@@ -1,7 +1,11 @@
 package com.tickeTeam.domain.game.repository;
 
 import com.tickeTeam.domain.game.entity.Game;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
+
+    List<Game> findByMatchDayBetweenAndStadium_Id(LocalDate startDate, LocalDate endDate, Long stadium_id);
 }
