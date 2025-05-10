7be1327b322074145db84ac4f08dc77800212e37
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-05-10 17:45:09 +0900

    refactor: 좌석 타입 enum으로 추출 및 좌석 번호 Integer타입으로 변경

--- src/main/java/com/tickeTeam/domain/seat/entity/SeatInfo.java
+++ src/main/java/com/tickeTeam/domain/seat/entity/SeatInfo.java
@@ -2,14 +2,21 @@ package com.tickeTeam.domain.seat.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SeatInfo {
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "seat_type", nullable = false)
-    private String seatType;
+    private SeatType seatType;
 
     @Column(name = "seat_section", nullable = false)
     private String seatSection;
@@ -21,5 +28,14 @@ public class SeatInfo {
     private String seatRow;
 
     @Column(name = "seat_num")
-    private int seatNum;
+    private Integer seatNum;
+
+    @Builder
+    public SeatInfo(SeatType seatType, String seatSection, String seatBlock, String seatRow, Integer seatNum) {
+        this.seatType = seatType;
+        this.seatSection = seatSection;
+        this.seatBlock = seatBlock;
+        this.seatRow = seatRow;
+        this.seatNum = seatNum;
+    }
 }
---
+++ src/main/java/com/tickeTeam/domain/seat/entity/SeatType.java
@@ -0,0 +1,6 @@
+package com.tickeTeam.domain.seat.entity;
+
+public enum SeatType {
+    ASSIGNED, // 지정석
+    NON_ASSIGNED // 비지정석
+}
