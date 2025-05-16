97d2b7cb7d7afe96bfaefcc9e7b65cbe7ef1c22d
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-05-16 23:47:37 +0900

    fix: Game 관련 오류 코드 수정

--- src/main/java/com/tickeTeam/common/exception/ErrorCode.java
+++ src/main/java/com/tickeTeam/common/exception/ErrorCode.java
@@ -17,8 +17,8 @@ public enum ErrorCode {
 
     // Game
     GAME_DATA_INSERT_ERROR(500,"GA001", "game 데이터 삽입 중 오류 발생"),
-    MATCHES_NOT_FOUND(404, "GA002", "일주일 내 경기들을 찾을 수 없음"),
-    MATCH_NOT_FOUND(404, "GA003", "해당 경기를 찾을 수 없음"),
+    MATCH_NOT_FOUND(404, "GA002", "해당 경기를 찾을 수 없음"),
+
     // Team
     TEAM_NOT_FOUND(404,"T001","팀 찾을 수 없음"),
 
