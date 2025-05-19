c71d00d9108cea204e904b5182c37d81c414a588
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-05-19 17:20:47 +0900

    fix: 잘못된 연관관계 수정

--- src/main/java/com/tickeTeam/domain/sectionPrice/entity/SectionPrice.java
+++ src/main/java/com/tickeTeam/domain/sectionPrice/entity/SectionPrice.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -31,7 +32,7 @@ public class SectionPrice {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "section_stadium")
     private Stadium sectionStadium;
 
