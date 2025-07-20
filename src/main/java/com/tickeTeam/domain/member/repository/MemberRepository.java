023d196812b048af235a0f473bbee41f5e0635bb
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-07-21 02:25:38 +0900

    fix: 불필요한 캐시 제거

--- src/main/java/com/tickeTeam/domain/member/repository/MemberRepository.java
+++ src/main/java/com/tickeTeam/domain/member/repository/MemberRepository.java
@@ -13,7 +13,6 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     Optional<Member> findByEmail(String email);
 
-    @Cacheable(value = "members", key = "#email")
     @Query("SELECT m FROM Member m JOIN FETCH m.favoriteTeam WHERE m.email = :email")
     Optional<Member> findByEmailWithTeam(@Param("email") String email);
 }
